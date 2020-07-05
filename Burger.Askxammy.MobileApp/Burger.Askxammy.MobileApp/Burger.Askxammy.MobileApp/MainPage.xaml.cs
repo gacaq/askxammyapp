84680d1de371ed60c6dd410cbb5ba0558cb4e82c
Germán Alonso Carranza Quiceno <German_Carranza@satrack.com>
2020-07-05 16:14:09 -0500

    Stacklayout and gridlayout example

---
+++ Burger.Askxammy.MobileApp/Burger.Askxammy.MobileApp/Burger.Askxammy.MobileApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Burger.Askxammy.MobileApp.Pages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Burger.Askxammy.MobileApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonLayout_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new StackLayoutExample());
+        }
+
+        private async void ButtonGrid_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GridLayoutExample());
+        }
+    }
+}
---
+++ Burger.Askxammy.MobileApp/Burger.Askxammy.MobileApp/Burger.Askxammy.MobileApp/Pages/StackLayoutExample.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Burger.Askxammy.MobileApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StackLayoutExample : ContentPage
+    {
+        public StackLayoutExample()
+        {
+            InitializeComponent();
+        }
+    }
+}
